e195f542795e59c8fb230de33ae23b9582cbdfe7
Christopher Rajon Polanco <102934273+ChrisARP12@users.noreply.github.com>
2023-12-12 20:13:45 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default ({ mode }) => {
   const host = process.env.HOST || '0.0.0.0';
-  const port = process.env.PORT || 3000;}
+  const port = process.env.PORT || 3000;
 
   return defineConfig({
     plugins: [react()],
