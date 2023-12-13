7aad76668cd33529f07aabc50c99c7ad64a4dd7a
Christopher Rajon Polanco <102934273+ChrisARP12@users.noreply.github.com>
2023-12-13 16:54:40 -0600

    Update juego.jsx

--- src/juego.jsx
+++ src/juego.jsx
@@ -1,34 +1,46 @@
 import { useEffect } from "react";
 
-export function init() {
-    useEffect(() => {
-        async function initPhaser() {
-            const Phaser = await import("phaser");
-            const { _lista_escenas } = await import("./clases_juego/manejadores/importador_escenas");
+import "./css/juego.css";
 
-            const phaserGame = new Phaser.Game({
-                type: Phaser.AUTO,
-                width: 1548,
-                height: 792,
-                scale: {
-                    mode: Phaser.Scale.FIT,
-                    autoCenter: Phaser.Scale.CENTER_BOTH,
-                },
-                dom: {
-                    createContainer: true,
-                },
-                parent: "contenedor_juego",
-                scene: _lista_escenas,
-                physics: {
-                    default: "arcade",
-                    arcade: {
-                        gravity: { y: 0 },
-                        debug: false,
-                    },
-                },
-                backgroundColor: "#000000",
-            });
-        }
-        initPhaser();
-    }, []);
+export default function Juego() {
+	useEffect(() => {
+		async function initPhaser() {
+			const Phaser = await import("phaser");
+			const { _lista_escenas } = await import("./clases_juego/manejadores/importador_escenas");
+
+			const phaserGame = new Phaser.Game({
+				type: Phaser.AUTO,
+				width: 1548,
+				height: 792,
+				scale: {
+					mode: Phaser.Scale.FIT,
+					autoCenter: Phaser.Scale.CENTER_BOTH,
+				},
+				dom: {
+					createContainer: true,
+				},
+				parent: "contenedor_juego",
+				scene: _lista_escenas,
+				physics: {
+					default: "arcade",
+					arcade: {
+						gravity: { y: 0 },
+						debug: false,
+					},
+				},
+				backgroundColor: "#000000",
+			});
+		}
+		initPhaser();
+	}, []);
+
+	return (
+		<>
+			<div
+				id="contenedor_juego"
+				key="contenedor_juego"
+				className="contenedorJuego"
+			></div>
+		</>
+	);
 }
