0da41e645c906c3f81bb17932bf8fd81fdbb6b6e
Christopher Rajon Polanco <102934273+ChrisARP12@users.noreply.github.com>
2023-12-13 16:34:08 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import Juego from './juego.jsx'
+// import ReactDOM from 'react-dom/client' // Elimina esta línea si no la necesitas
+import Juego from './juego.jsx';
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <Juego/>
+// );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Juego/>
-)
+// Inicializa el juego directamente
+const initGame = () => {
+    // Aquí puedes llamar a la función de inicialización del juego si es necesario
+    // Ejemplo: Juego.init();
+};
+
+// Llama a la función de inicialización del juego al cargar la página
+document.addEventListener('DOMContentLoaded', initGame);
+
