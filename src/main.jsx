011f18cd0cf1a7b3c7ae12a823bbcd9200bcfb3d
Christopher Rajon Polanco <102934273+ChrisARP12@users.noreply.github.com>
2023-12-12 20:41:22 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom/client'
 import Juego from './juego.jsx'
 
-ReactDOM.render(<Juego />, document.getElementById('root'));
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Juego/>
+)
