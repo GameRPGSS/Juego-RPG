28a4c305afaa41c3c35af49063d834adb8198daf
Christopher Rajon Polanco <102934273+ChrisARP12@users.noreply.github.com>
2023-12-12 20:06:01 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  assetsDir: 'assets',
-  server: {
-    port: 443 // Cambiar el puerto por defecto
-  }
-})
+export default ({ mode }) => {
+  const port = process.env.PORT || 3000; // Usa el puerto de Render o el 3000 por defecto
+
+  return defineConfig({
+    plugins: [react()],
+    assetsDir: 'assets',
+    server: {
+      port,
+    },
+  });
+};
+
